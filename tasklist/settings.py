1859aec01a50b099a89d6b3fa38650f4cd1cd662
Rag Sagar.V <ragsagar@gmail.com>
2014-04-03 12:10:16 +0400

    Checking different settings

--- tasklist/settings.py
+++ tasklist/settings.py
@@ -62,8 +62,11 @@ WSGI_APPLICATION = 'tasklist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    #'default': {
+    'default':dj_database_url.config()
+    #{
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #}
@@ -116,5 +119,3 @@ LOGIN_REDIRECT_URL = reverse('list_tasks')
 
 SITE_ID = 1
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
