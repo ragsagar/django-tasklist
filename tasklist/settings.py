22708d56dc9ba3972c44ca6701a5541fcf017303
Rag Sagar.V <ragsagar@gmail.com>
2014-04-03 12:07:27 +0400

    Commenting exisint database settings

--- tasklist/settings.py
+++ tasklist/settings.py
@@ -63,10 +63,10 @@ WSGI_APPLICATION = 'tasklist.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 }
 
 # Internationalization
