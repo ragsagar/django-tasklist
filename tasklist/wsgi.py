ecb448bf5050fa184eaf11654b7e4e4be4088c3e
Rag Sagar.V <ragsagar@gmail.com>
2014-04-03 12:36:57 +0400

    Reverting wsgi file

--- tasklist/wsgi.py
+++ tasklist/wsgi.py
@@ -11,6 +11,4 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tasklist.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
