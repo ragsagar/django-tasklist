63babab37ef4cae6c5132b7174c400788e38abab
Rag Sagar.V <ragsagar@gmail.com>
2014-04-04 16:23:37 +0400

    Adding test for task detail view

--- tasks/tests.py
+++ tasks/tests.py
@@ -21,14 +21,24 @@ class TaskTestCase(TestCase):
                 'description': 'testing task',
                 'assigned_user': user,
                 }
-        Task.objects.create(**data)
+        self.task = Task.objects.create(**data)
+        self.client.login(username='ragsagar', password='password')
 
     def test_list_tasks(self):
         """
         Tests for the view to list all tasks.
         """
         list_tasks_url = reverse('list_tasks')
-        self.client.login(username='ragsagar', password='password')
         response = self.client.get(list_tasks_url)
         self.assertEqual(response.status_code, 200)
+        tasks = Task.objects.all()
+        self.assertEqual(len(response.context_data['task_list']), tasks.count())
 
+    def test_detail_task(self):
+        """
+        Test detail task view page.
+        """
+        detail_url = reverse('task_detail', kwargs={'pk': self.task.pk})
+        response = self.client.get(detail_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context_data['task'], self.task)
