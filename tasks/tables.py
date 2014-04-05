a1b244529febd1fcc77103077923a1fd4902bd8f
Rag Sagar.V <ragsagar@gmail.com>
2014-04-06 00:21:12 +0400

    Showing full name in table listing if available

--- tasks/tables.py
+++ tasks/tables.py
@@ -6,6 +6,12 @@ from .models import Task
 class TaskTable(tables.Table):
     id = tables.LinkColumn('task_detail', args=[A('pk')])
 
+    def render_assigned_user(self, value):
+        """
+        Show full name if available.
+        """
+        return value.get_full_name() or value
+
     class Meta:
         model = Task
         attrs = {'class': 'table table-condensed rowlink', }
