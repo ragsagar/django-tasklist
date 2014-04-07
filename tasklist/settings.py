7a7f9af2cbec73173f2f5a5e898157913f6e6c12
Rag Sagar.V <ragsagar@gmail.com>
2014-04-07 08:13:05 +0400

    Adding humanize to installed apps

--- tasklist/settings.py
+++ tasklist/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.comments',
+    'django.contrib.humanize',
     'crispy_forms',
     'django_tables2',
     'tasks',
