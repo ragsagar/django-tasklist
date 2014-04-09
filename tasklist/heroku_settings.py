d3c140facbb36106b2c189c4f80aac357faec447
Rag Sagar.V <ragsagar@gmail.com>
2014-04-09 15:16:51 +0400

    Disabling debug in heroku,also passing default database to use as sqlite

--- tasklist/heroku_settings.py
+++ tasklist/heroku_settings.py
@@ -2,10 +2,9 @@ from tasklist.settings import *
 
 import dj_database_url
 
+DEBUG = True
+TEMPLATE_DEBUG = True
+
 DATABASES = {
-    'default':dj_database_url.config()
-    #{
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+    'default':dj_database_url.config(default='sqlite:////' + SQLITE_DB)
 }
