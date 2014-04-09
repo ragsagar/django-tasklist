5f1af405eaade812542e85fe5f3f7acfc917816e
Rag Sagar.V <ragsagar@gmail.com>
2014-04-09 15:40:07 +0400

    Turning off debug in heroku

--- tasklist/heroku_settings.py
+++ tasklist/heroku_settings.py
@@ -2,8 +2,9 @@ from tasklist.settings import *
 
 import dj_database_url
 
-DEBUG = True
-TEMPLATE_DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = False
+
 
 DATABASES = {
     'default':dj_database_url.config(default='sqlite:////' + SQLITE_DB)
